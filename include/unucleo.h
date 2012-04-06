0c0686ade152e4d227dc596827fe1fdae7495275
germanoa@gmail.com <germanoa@gmail.com@452e7c17-1804-702a-e3c7-0b90639d8dad>
2012-04-06 05:04:31 +0000

    Estrutura de diretorios base.

--- include/unucleo.h
+++ include/unucleo.h
@@ -1,3 +1,13 @@
+
+
+struct proc_struct {
+    int pid;
+    int state;  /* 0-ready  1-running   2-blocked */
+    int prio;   /* 0-alta 1-media 2-baixa */
+    int context;
+};
+
+
 int libsisop_init();
 int mproc_create(int prio, void * (*start_routine)(void*), void * arg);
 void mproc_yield(void);
